64e334872d463d9a015f015249c57542451204b0
unknown <Jeremy.Magee@CA2021559W1.ey.net>
2015-04-21 19:52:49 -0300

    Initial commit. Committing all code

---
+++ ProjectUpload/Controllers/DocumentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectUpload.Controllers
+{
+    public class DocumentController : Controller
+    {
+        // GET: Document
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Upload(String temp)
+        {
+            return null;
+        }
+    }
+}
---
+++ ProjectUpload/Models/Document.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjectUpload.Models
+{
+    public class Document
+    {
+        public int ID { get; set; }
+        public string TestName { get; set; }
+        public string CourseName { get; set; }
+        public string CourseNum { get; set; }
+        public string Semester { get; set; }
+        public int Year { get; set; }
+        public string AnswersIncluded { get; set; }
+    }
+
+    public class DocumentDbContext : DbContext
+    {
+        public DbSet<Document> Documents { get; set; } 
+    }
+}
